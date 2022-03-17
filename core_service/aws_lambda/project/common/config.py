a88a2639495e3c666e957de8411be512e4ae9217
dachanh <ghostcodert7@gmail.com>
2022-03-18 00:27:34 +0700

    UPDATE: endpoint webhook

--- core_service/aws_lambda/project/common/config.py
+++ core_service/aws_lambda/project/common/config.py
@@ -5,5 +5,9 @@ IDENTITYPOOLID = "us-east-2:fa0b76bc-01fa-4bb8-b7cf-a5000954aafb"
 SITEKEYGOOGLE = "6LcqEGMeAAAAAAEDnBue7fwR4pmvNO7JKWkHtAjl"
 SECRETKEYGOOGLE = "6LcqEGMeAAAAAOiJAMcg1NNfj6eA62gQPLJAtQMt"
 ENDPOINTCAPTCHAVERIFY = "https://www.google.com/recaptcha/api/siteverify"
-WEBHOOK="https://hooks.slack.com/services/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"
-CHANNELWEBHOOK="#feedback-daita"
+# Github OpenID wrapper
+# Change these if used with GitHub Enterprise (see below)
+GITHUB_API_URL = "https://api.github.com"
+GITHUB_LOGIN_URL = "https://github.com"
+WEBHOOK="https://hooks.slack.com/services/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"
+CHANNELWEBHOOK="#user-feedback"
