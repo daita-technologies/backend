06bc417c849e96bb76e0c0d4cc5382026370b648
dachanh <ghostcodert7@gmail.com>
2022-03-27 09:53:11 +0700

    Feature: prepare_step

--- ai_caller_service_v2/daita-caller-service-app/functions/handlers/prepare_step/app.py
+++ ai_caller_service_v2/daita-caller-service-app/functions/handlers/prepare_step/app.py
@@ -1,8 +1,12 @@
 import time
 import json
+from config import *
+from response import *
+from images import ImageLoader
 
 def lambda_handler(event, context):
-
+    body =  json.load(event['detail'])
+    imageloader = ImageLoader()
 
     return {
         "statusCode": 200,
