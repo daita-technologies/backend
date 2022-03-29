1425c623f178ebb7a56c8a7d720b532258767a65
dachanh <ghostcodert7@gmail.com>
2022-03-29 21:44:15 +0700

    feat: prepare_step

---
+++ daita-app/shared-layer/commons/python/lambda_base_class.py
@@ -0,0 +1,37 @@
+import logging
+import os
+from identity_check import *
+import json
+
+class LambdaBaseClass(object):
+    
+    def __init__(self) -> None:
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(os.environ["LOGGING"])
+
+    def handle(self, event, context):        
+        raise NotImplementedError
+
+    @classmethod
+    def parse_body(cls, func):
+        def parser(object, event):
+            body = json.loads(event['body'])
+            object.logger.info("Body: {}".format(body))
+            try:
+                print("Before call func")
+                func(object, body)
+            except Exception as e:
+                raise Exception(MESS_INVALID_JSON_INPUT) from e
+            object._check_input_value()
+            return 
+        
+        return parser 
+
+    def _check_input_value(self):        
+        pass
+        return
+
+    def get_identity(self, id_token):
+        identity = aws_get_identity_id(id_token)
+        self.logger.info(f"identity: {identity}")
+        return identity
