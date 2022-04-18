4ef840822263f98e8ccd5febce2ea8b38b4b81c4
teppialy <trantuananh2006@gmail.com>
2022-04-18 07:59:21 +0000

    update(generate): not use generate build in old version

--- core_service/aws_lambda/project/de_lambda.py
+++ core_service/aws_lambda/project/de_lambda.py
@@ -1,7 +1,7 @@
 from aws_lambda.lambda_service import AWSLambdaService
 from aws_lambda.project.de_lambda_project_funcs import deploy_lambda_project
-from aws_lambda.project.de_lambda_generate_funcs import deploy_lambda_generate
-from aws_lambda.project.de_lambda_balancer_funcs import deploy_lambda_balancer
+# from aws_lambda.project.de_lambda_generate_funcs import deploy_lambda_generate
+# from aws_lambda.project.de_lambda_balancer_funcs import deploy_lambda_balancer
 from aws_lambda.project.de_lambda_auth_funcs import deploy_lambda_auth
 
 from aws_lambda.project.de_lambda_webhook import deploy_lambda_webhook
@@ -12,8 +12,8 @@ def deploy_lambda(general_info):
     ls_lambda_val = []
 
     ls_lambda_val += deploy_lambda_project(general_info, lambda_service)
-    ls_lambda_val += deploy_lambda_generate(general_info, lambda_service)
-    ls_lambda_val += deploy_lambda_balancer(general_info, lambda_service)
+    # ls_lambda_val += deploy_lambda_generate(general_info, lambda_service)
+    # ls_lambda_val += deploy_lambda_balancer(general_info, lambda_service)
     ls_lambda_val += deploy_lambda_auth(general_info, lambda_service)
     ls_lambda_val += deploy_lambda_webhook(general_info, lambda_service)
     ls_lambda_val += deploy_lambda_send_mail(general_info, lambda_service)    
