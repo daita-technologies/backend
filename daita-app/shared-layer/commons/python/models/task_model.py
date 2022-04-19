53289106fa4b4703ae31169eac18d1337f27bcdf
teppialy <trantuananh2006@gmail.com>
2022-04-19 16:41:36 +0000

    Merge branch 'update/taskdashboard_sorting'

--- daita-app/shared-layer/commons/python/models/task_model.py
+++ daita-app/shared-layer/commons/python/models/task_model.py
@@ -20,49 +20,61 @@ class TaskModel():
     def __init__(self, table_name) -> None:
         self.table = boto3.resource('dynamodb').Table(table_name) 
 
-    def _query_task(self, identity_id, filter_exp, pag_page_token, limit_size, projection_str="", att_name = {}):
+    def _query_w_option(self, key_condition_exp, filter_exp, limit_size, projection_str, attr_name,
+                         ex_start_key=None, index_table = None, text_print = "response from query"):
+        
+        dict_argv = {
+            "KeyConditionExpression": key_condition_exp,
+            "FilterExpression": filter_exp,
+            "Limit": limit_size,
+            "ProjectionExpression": projection_str,
+            "ExpressionAttributeNames": attr_name,
+            "ScanIndexForward": False,
+            "ExclusiveStartKey": ex_start_key,
+            "IndexName": index_table
+        }
+        if ex_start_key is None:
+            dict_argv.pop("ExclusiveStartKey", None)
+        if index_table is None:
+            dict_argv.pop("IndexName", None)
+
+        response = self.table.query(**dict_argv)
+
+        print(f"{text_print}: \n {response}")
+
+        return response
+
+    def _query_task(self, pag_page_token, key_condition_exp, filter_exp, limit_size, projection_str="", attr_name={},
+                    index_table=None, text_print="response from query"):
         if pag_page_token is None:
             ls_page_token = []
             ### get all task with paginator
-            response = self.table.query (
-                        KeyConditionExpression=Key(self.FIELD_IDENTITY_ID).eq(identity_id),
-                        FilterExpression=filter_exp,
-                        Limit = limit_size,
-                        ProjectionExpression= projection_str,
-                        ExpressionAttributeNames = att_name                        
-                    )
+            response = self._query_w_option(key_condition_exp, filter_exp, limit_size, projection_str, attr_name,
+                                            ex_start_key=None, index_table=index_table, text_print="response from query")            
             do_continue = response.get('LastEvaluatedKey')
             return_ls_items = response.get('Items', [])
 
             while do_continue:
                 ls_page_token.append(do_continue)
-                response = self.table.query (
-                        KeyConditionExpression=Key(self.FIELD_IDENTITY_ID).eq(identity_id),
-                        FilterExpression=filter_exp,
-                        Limit = limit_size,
-                        ExclusiveStartKey = do_continue,
-                        ProjectionExpression = projection_str,
-                        ExpressionAttributeNames = att_name
-                    )
+                response = self._query_w_option(key_condition_exp, filter_exp, limit_size, projection_str, attr_name,
+                                                ex_start_key=do_continue, index_table=index_table, text_print="response from query")                
                 do_continue = response.get('LastEvaluatedKey')
             return (return_ls_items, ls_page_token)
         else:
-            response = self.table.query (
-                        KeyConditionExpression=Key(self.FIELD_IDENTITY_ID).eq(identity_id),
-                        FilterExpression=filter_exp,
-                        Limit = limit_size,
-                        ExclusiveStartKey = pag_page_token,
-                        ProjectionExpression = projection_str,
-                        ExpressionAttributeNames = att_name
-                    )
+            response = self._query_w_option(key_condition_exp, filter_exp, limit_size, projection_str, attr_name,
+                                            ex_start_key=pag_page_token, index_table=index_table, text_print="response from query")
+            
             return (response.get('Items', []), [])
 
     def get_tasks_w(self, identity_id, filter_prj_id, filter_ls_status, filter_process_type, pag_page_token, limit_size):
         filterExpression = Attr(self.FIELD_PROCESS_TYPE).eq(filter_process_type)
         if len(filter_prj_id) == 0:
             filterExpression = filterExpression & Attr(self.FIELD_PROJECT_ID).exists()
+            key_condition_exp = Key(self.FIELD_IDENTITY_ID).eq(identity_id)
+            index_table = None
         else:
-            filterExpression = filterExpression & Attr(self.FIELD_PROJECT_ID).eq(filter_prj_id)
+            key_condition_exp = Key(self.FIELD_PROJECT_ID).eq(filter_prj_id)
+            index_table = 
         
         if len(filter_ls_status) == 0:
             filterExpression = filterExpression & Attr(self.FIELD_STATUS).exists()
@@ -88,26 +100,28 @@ class TaskModel():
         attr_name = {
             "#sta": self.FIELD_STATUS
         }
-        ls_task, ls_page_token = self._query_task(identity_id, filterExpression, pag_page_token, limit_size, projecttion_str, attr_name)
+
+        ls_task, ls_page_token = self._query_task(pag_page_token, key_condition_exp, filterExpression, limit_size, projecttion_str, attr_name,
+                                                  index_table)
 
         return ls_task, ls_page_token
 
     def update_status(self, task_id, identity_id, status):
         response = self.table.update_item(
-                    Key={
-                            self.FIELD_TASK_ID: task_id,
-                            self.FIELD_IDENTITY_ID: identity_id
-                        },
-                    UpdateExpression="SET #s=:s, #updated_time=:updated_time",
-                    ExpressionAttributeValues={
-                        ':s': status,
-                        ':updated_time': convert_current_date_to_iso8601()
-                    },
-                    ExpressionAttributeNames={
-                        '#s' : self.FIELD_STATUS,
-                        '#updated_time': self.FIELD_UPDATED_TIME,
-                    }
-                )
+            Key={
+                self.FIELD_TASK_ID: task_id,
+                self.FIELD_IDENTITY_ID: identity_id
+            },
+            UpdateExpression="SET #s=:s, #updated_time=:updated_time",
+            ExpressionAttributeValues={
+                ':s': status,
+                ':updated_time': convert_current_date_to_iso8601()
+            },
+            ExpressionAttributeNames={
+                '#s': self.FIELD_STATUS,
+                '#updated_time': self.FIELD_UPDATED_TIME,
+            }
+        )
         
         return response
 
