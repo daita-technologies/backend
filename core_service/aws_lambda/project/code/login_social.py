7fa2724b157a355223827cfac9cf6e7ca391989e
BEdaita <102169046+BEdaita@users.noreply.github.com>
2022-05-27 00:52:21 +0700

    Feat/change url (#101)
    * fix: change url
    
    * Update: decrease number worker request AI

--- core_service/aws_lambda/project/code/login_social.py
+++ core_service/aws_lambda/project/code/login_social.py
@@ -31,7 +31,7 @@ def lambda_handler(event, context):
     except Exception as e:
         print(e)
         if 'error_description' in param:
-            location = 'https://dev.daita.tech/'
+            location = 'https://app.daita.tech/'
             headers = {"Location":location,	"Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT"}
             return {
                     "statusCode": 302,
