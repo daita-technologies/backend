05bec2f6c0513981c22b8b24c975fc5adc6a11a3
teppialy <trantuananh2006@gmail.com>
2022-06-05 17:48:58 +0000

    fix: (download) process download for large amount of data

--- daita-app/dataflow-service/compress-download-app/functions/divide_download_keys/app.py
+++ daita-app/dataflow-service/compress-download-app/functions/divide_download_keys/app.py
@@ -1,6 +1,8 @@
 import os
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
+import json
+from utils import get_bucket_key_from_s3_uri, split_ls_into_batch
 
 
 CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", 8))
@@ -8,6 +10,7 @@ db_resource = boto3.resource("dynamodb")
 data_original_table = db_resource.Table(os.getenv("TableDataOriginal"))
 data_augment_table = db_resource.Table(os.getenv("TableDataAugment"))
 data_preprocess_table = db_resource.Table(os.getenv("TableDataPreprocess"))
+s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
@@ -38,7 +41,7 @@ def lambda_handler(event, context):
     for table in ls_table:
         response = table.query(
                 KeyConditionExpression = Key('project_id').eq(project_id),
-                ProjectionExpression='filename, s3_key, classtype, gen_id, type_method, size',
+                ProjectionExpression='filename, s3_key, classtype, gen_id, type_method',
                 Limit = 500
             )
         ls_object = ls_object + response['Items']
@@ -47,7 +50,7 @@ def lambda_handler(event, context):
         while next_token is not None:
             response = table.query(
                 KeyConditionExpression = Key('project_id').eq(project_id),
-                ProjectionExpression='filename, s3_key, classtype, gen_id, type_method, size',
+                ProjectionExpression='filename, s3_key, classtype, gen_id, type_method',
                 Limit = 500,
                 ExclusiveStartKey=next_token,
             )
@@ -62,8 +65,23 @@ def lambda_handler(event, context):
     file_chunks = []
     for size in range(0, len(ls_object), CHUNK_SIZE):
         file_chunks.append(ls_object[size:size + CHUNK_SIZE])
+        
+    if len(ls_object)>0:
+        bucket, folder = get_bucket_key_from_s3_uri(ls_object[0]["s3_key"])
+        folder = "/".join(folder.split("/")[:-1])
+        s3_key_path = os.path.join(folder, f"download_state_json/{task_id}/download_devide.json")
+        s3.put_object(
+                Body=json.dumps(file_chunks),
+                Bucket= bucket,
+                Key= s3_key_path
+            )            
+    else:
+        bucket = None
+        s3_key_path = None
 
     return {
-        "file_chunks": file_chunks,
+        "file_chunks": list(range(len(file_chunks))),
+        "bucket": bucket,
+        "s3_key_path": s3_key_path,
         "workdir": task_id,
     }
