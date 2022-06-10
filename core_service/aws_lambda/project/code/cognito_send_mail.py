86a20dd4974a7b77001a15ce86fc4e3d7c94dd1d
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2022-06-10 09:25:43 +0200

    content of the message, in text format
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- core_service/aws_lambda/project/code/cognito_send_mail.py
+++ core_service/aws_lambda/project/code/cognito_send_mail.py
@@ -7,70 +7,60 @@ import os
 import uuid
 from botocore.exceptions import ClientError
 
+
 def convert_response(data):
     return {
         "statusCode": 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
         },
-        "body": json.dumps(data),        
+        "body": json.dumps(data),
     }
 
 
-    
 def lambda_handler(event, context):
-    try :
-        destination_email = event['destination_email']
-        message_email =  event['message_email']
-        subject = event['subject']
+    try:
+        destination_email = event["destination_email"]
+        message_email = event["message_email"]
+        message_email_text = event["message_email_text"]
+        subject = event["subject"]
     except Exception as e:
-         return convert_response({"error": True, 
-                 "success": False, 
-                 "message": repr(e), 
-                 "data": None})
+        return convert_response(
+            {"error": True, "success": False, "message": repr(e), "data": None}
+        )
 
-    client = boto3.client('ses')
+    client = boto3.client("ses")
     print("send mail")
     try:
         response = client.send_email(
             Destination={
-                'ToAddresses':[
-                    destination_email
-                ],
+                "ToAddresses": [destination_email],
             },
             Message={
-                'Body':{
-                    'Html':{
-                        'Charset':'UTF-8',
-                        'Data': message_email,
-    
+                "Body": {
+                    "Html": {
+                        "Charset": "UTF-8",
+                        "Data": message_email,
                     },
-                    'Text':{
-                        'Charset':'UTF-8',
-                        'Data':'confirmation email'
-                    }
-                   
-                }, 
-                'Subject':{
-                        'Charset':'UTF-8',
-                        'Data':subject
+                    "Text": {"Charset": "UTF-8", "Data": message_email_text},
                 },
+                "Subject": {"Charset": "UTF-8", "Data": subject},
             },
             Source="DAITA Team <noreply@daita.tech>",
-            
         )
     except ClientError as e:
-         print(e)
-         return convert_response({"error": True, 
-                 "success": False, 
-                 "message": e, 
-                 "data": None})
-    print(response['MessageId'])
-    return convert_response({
-        "error": False,
-        "success":True,
-        "message": response['MessageId'],
-        "data":None
-    })
+        print(e)
+        return convert_response(
+            {"error": True, "success": False, "message": e, "data": None}
+        )
+    print(response["MessageId"])
+    return convert_response(
+        {
+            "error": False,
+            "success": True,
+            "message": response["MessageId"],
+            "data": None,
+        }
+    )
--- daita-app/dataflow-service/compress-download-app/functions/send_completed_email/app.py
+++ daita-app/dataflow-service/compress-download-app/functions/send_completed_email/app.py
@@ -4,57 +4,59 @@ import base64
 import boto3
 
 
-client = boto3.client('ses')
+client = boto3.client("ses")
 
 
 def get_email(id_token):
     header, payload, signature = id_token.split(".")
-    user_info = json.loads(base64.b64decode(payload + "="*10)) #add missing padding
+    user_info = json.loads(base64.b64decode(payload + "=" * 10))  # add missing padding
     return user_info["email"]
 
 
 def lambda_handler(event, context):
     email = get_email(event["id_token"])
     print("send mail")
-    #TODO: change href to something for meaningful
-    message = '''
+    message = """
         <p>Dear User,</p>
         <p>Your download link has been created. Please log into DAITA Platform and go to <a href='https://app.daita.tech/task-list'>My Tasks</a> to download your files.</p>
         <p>Best,</p>
         <p>The DAITA Team</p>
         <p>---</p>
         <p><i>In case you encounter any issues or questions, please contact us at <a href = "mailto: contact@daita.tech">contact@daita.tech</a>.</i></p>
-        '''
+        """
+    message_email_text = """
+        Dear User,
+        Your download link has been created. Please log into DAITA Platform and go to https://app.daita.tech/task-list to download your files.
+        Best,
+        The DAITA Team
+        ---
+        In case you encounter any issues or questions, please contact us at contact@daita.tech.
+        """
     response = client.send_email(
         Destination={
-            'ToAddresses':[
-                email
-            ],
+            "ToAddresses": [email],
         },
         Message={
-            'Subject':{
-                    'Charset':'UTF-8',
-                    'Data':"Your files are ready for download"
+            "Subject": {
+                "Charset": "UTF-8",
+                "Data": "Your files are ready for download",
             },
-            'Body':{
-                'Html':{
-                    'Charset':'UTF-8',
-                    'Data': message,
-
+            "Body": {
+                "Html": {
+                    "Charset": "UTF-8",
+                    "Data": message,
                 },
-                'Text':{
-                    'Charset':'UTF-8',
-                    'Data':"Your files are ready for download"
-                }
-
+                "Text": {
+                    "Charset": "UTF-8",
+                    "Data": message_email_text,
+                },
             },
         },
         Source="DAITA Team <noreply@daita.tech>",
-
     )
     return {
         "error": False,
-        "success":True,
-        "message": "Email sent! Message ID: {}".format(response['MessageId']),
-        "data": None
+        "success": True,
+        "message": "Email sent! Message ID: {}".format(response["MessageId"]),
+        "data": None,
     }
