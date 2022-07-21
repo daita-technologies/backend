49a96cbdaf0488be68dabc774e0412830d4708c2
BEdaita <daitaservices@gmail.com>
2022-07-21 04:03:32 +0000

    Update: auth services sam app

---
+++ daita-app/auth-service/functions/github_openid_userinfo_wrapper/app.py
@@ -0,0 +1,50 @@
+import json
+import requests
+
+from config import *
+from response import error_response
+
+
+OAUTH_USERINFO_URL = "user"
+OAUTH_USER_EMAIL_URL = "user/emails"
+
+
+@error_response
+def lambda_handler(event, context):
+    oauth_token = event["headers"].get("Authorization").split("Bearer ")[1]
+    userinfo_response = requests.get(
+        url=f"{GITHUB_API_URL}/{OAUTH_USERINFO_URL}",
+        headers={
+            "Authorization": f"token {oauth_token}",
+            "Accept": "application/json"
+        },
+        allow_redirects=False
+    )
+
+    useremail_response = requests.get(
+        url=f"{GITHUB_API_URL}/{OAUTH_USER_EMAIL_URL}",
+        headers={
+            "Authorization": f"token {oauth_token}",
+            "Accept": "application/json"
+        },
+        allow_redirects=False
+    )
+    useremails = useremail_response.json()
+    for email in useremails:
+        if email["primary"]:
+            primary_email = email["email"]
+            break
+    else:
+        primary_email = None
+
+    body = userinfo_response.json()
+    body["email"] = primary_email
+    return {
+        "body": json.dumps(
+            {
+                **body,
+                "sub": body["id"]
+            }
+        ),
+        "isBase64Encoded": False
+    }
---
+++ daita-app/auth-service/functions/sign_up/app.py
@@ -0,0 +1,123 @@
+from cmath import inf
+import os
+import json
+import logging
+from http import HTTPStatus
+import os
+import boto3
+from dataclasses import dataclass
+import re
+
+from error import *
+from response import *
+from config import *
+from custom_mail import *
+from verify_captcha import *
+
+from datetime import datetime
+
+
+def convert_current_date_to_iso8601():
+    my_date = datetime.now()
+    return my_date.isoformat()
+
+
+cog_provider_client = boto3.client("cognito-idp")
+cog_identity_client = boto3.client("cognito-identity")
+RESPONSE_HEADER = {
+    "Access-Control-Allow-Creentials": "true",
+    "Access-Control-Allow-Methods": "GET, HEAD, OPTIONS, POST, PUT",
+}
+
+
+PASSWORD_REGEX = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\^$*.\[\]{}\(\)?\-\"!@#%&\/,><\':;|_~`])\S{8,99}$"
+
+
+class User(object):
+    def __init__(self):
+        self.db_client = boto3.resource("dynamodb", region_name=REGION)
+
+    def create_item(self, info):
+        self.db_client.Table("User").put_item(
+            Item={
+                "ID": info["ID"],
+                "username": info["username"],
+                "role": "normal",
+                "status": "deactivate",
+                "create_at": convert_current_date_to_iso8601(),
+            }
+        )
+
+
+def checkInvalidUserRegister(user, mail):
+    response = cog_provider_client.list_users(UserPoolId=USERPOOLID)
+    info_user = list(
+        filter(lambda x: x["Username"] == user, response["Users"]))
+    if len(info_user):
+        return False, True
+    for _, data in enumerate(response["Users"]):
+        for info in data["Attributes"]:
+            if info["Name"] == "email" and info["Value"] == mail:
+                return True, False
+    return True, True
+
+
+@error_response
+def lambda_handler(event, context):
+
+    try:
+        body = json.loads(event["body"])
+        username = body["username"]
+        mail = body["email"]
+        password = body["password"]
+        captcha = body["captcha"]
+    except Exception as e:
+        print(e)
+        raise Exception(e)
+
+    try:
+        verify_captcha(captcha)
+    except Exception as exc:
+        raise Exception(MessageCaptchaFailed) from exc
+
+    if not re.match(PASSWORD_REGEX, password):
+        raise Exception(MessageInvalidPassword)
+    checkUsername, checkemail = checkInvalidUserRegister(username, mail)
+
+    if not checkUsername:
+        raise Exception(MessageSignUpUsernameInvalid)
+    elif not checkemail:
+        raise Exception(MessageSignUPEmailInvalid)
+
+    try:
+        respUserSignUp = cog_provider_client.sign_up(
+            ClientId=CLIENTPOOLID,
+            Username=username,
+            Password=password,
+            UserAttributes=[{"Name": "email", "Value": mail}],
+        )
+    except Exception as e:
+        print(e)
+        raise Exception(MessageSignUpFailed)
+
+    model = User()
+    try:
+        model.create_item(
+            {"ID": respUserSignUp["UserSub"], "username": username})
+    except Exception as e:
+        print(e)
+        raise Exception(MessageSignUpFailed)
+
+    AddTriggerCustomMail(
+        {
+            "region": REGION,
+            "user": username,
+            "mail": mail,
+            "subject": "Your email confirmation code",
+        }
+    )
+    return generate_response(
+        message="Sign up for user {} was successful.".format(username),
+        data={},
+        headers=RESPONSE_HEADER,
+    )
