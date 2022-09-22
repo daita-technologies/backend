659d624d6fe920b9e3843ce8e2726deab79bd250
BEdaita <daitaservices@gmail.com>
2022-09-22 09:58:00 +0000

    fix: response github oauth2

--- daita-app/auth-service/CognitoUserPool/functions/github_openid_token_wrapper/app.py
+++ daita-app/auth-service/CognitoUserPool/functions/github_openid_token_wrapper/app.py
@@ -3,14 +3,16 @@ import requests
 
 from config import *
 from response import error_response
-
+import base64
 
 OAUTH_TOKEN_URL = "login/oauth/access_token"
 
 
 @error_response
 def lambda_handler(event, context):
-    print(event['body'])
+    print(event)
+    if event["isBase64Encoded"]:
+        event["body"] = base64.b64decode(event["body"]).decode("ascii")
     response = requests.post(
         url=f"{GITHUB_LOGIN_URL}/{OAUTH_TOKEN_URL}",
         headers={
@@ -20,7 +22,8 @@ def lambda_handler(event, context):
         data=event["body"],
     )
     print(response.json())
-    return {
-        "body": json.dumps(response.json()),
-        "isBase64Encoded": False
-    }
+    # return {
+    #     "body": json.dumps(response.json()),
+    #     "isBase64Encoded": False
+    # }
+    return json.dumps(response.json())
