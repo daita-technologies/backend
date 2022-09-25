aac1091d9e51adf0192d0bc1d7c18525c476338f
BEdaita <daitaservices@gmail.com>
2022-09-25 11:07:32 +0000

    Cconfig new env

---
+++ annotation-app/project-service/api-handler-functions/hdler_project_upload_update.py
@@ -0,0 +1,26 @@
+import boto3
+import json
+import os
+import uuid
+
+from config import *
+from response import *
+from error_messages import *
+from utils import convert_current_date_to_iso8601, aws_get_identity_id, get_num_prj
+import const
+
+
+from lambda_base_class import LambdaBaseClass
+
+
+class ProjectAnnotationUploadUpdate(LambdaBaseClass):
+    def __init__(self) -> None:
+        super().__init__()
+    
+
+    def handle(self,event,context):
+        return
+
+@error_response
+def lambda_handler(event, context):
+    return ProjectAnnotationUploadUpdate().handle(event=event,context=context)
