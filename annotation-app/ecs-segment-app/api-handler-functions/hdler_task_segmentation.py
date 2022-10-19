6f1aebcc59f2306ef85d6f01e473790ccd6311b0
BEdaita <102169046+BEdaita@users.noreply.github.com>
2022-10-19 22:18:23 +0700

    Feat/ecs segmentation (#201)
    * add template ecs task
    
    * feat: fix run ecs task with private subnet
    
    * Update: ecs-segmentation app
    
    * Update: ecs trigger
    
    * supdate: env variable
    
    * ecs app
    
    * change name sample image
    
    * update: ecs-segmentation
    
    * move ecs-app to annototation project
    
    * update : storage layer
    
    * add ec2 role
    
    * Update
    
    * Update
    
    * fix typos
    
    * delete unused file
    
    * update : connect efs to container
    
    * fix: typos
    
    * sqs trigger upload image
    
    * Update :ecs segmentation
    
    * devide batch on dynamodb
    
    Co-authored-by: teppialy <trantuananh2006@gmail.com>

---
+++ annotation-app/ecs-segment-app/api-handler-functions/hdler_task_segmentation.py
@@ -0,0 +1,65 @@
+import boto3
+import re
+import os
+import json
+import glob
+
+from response import *
+
+from lambda_base_class import LambdaBaseClass
+
+
+
+class EventTaskQueueSegmentationClass(LambdaBaseClass):
+    def __init__(self) -> None:
+        super().__init__()
+        self.s3 = boto3.client('s3')
+        dns = (os.environ['SQS_VPC_ENDPOINT']).split(":")[1]
+        self.sqs = boto3.client('sqs',endpoint_url='https://{}'.format(dns))
+        self.sqsResourse = boto3.resource('sqs',endpoint_url='https://{}'.format(dns))
+    
+    def handle(self,event,context):
+        for record in event['Records']:
+            body = json.loads(record['body'])
+            print(body)
+            OutDir =  glob.glob(os.path.join(os.environ['EFSPATH'],body['output_directory'])+'/*')
+            if len(OutDir) == len(body['output_directory']):
+                self.sqs.delete_message(
+                    QueueUrl=os.environ['QUEUE'],
+                    ReceiptHandle= record['receiptHandle']
+                )
+            else:
+                nbReplay = 0
+                if 'sqs-dlq-replay-nb' in record['messageAttributes']:
+                     nbReplay = int(record['messageAttributes']['sqs-dlq-replay-nb']["stringValue"])
+                print("nb_replay ",nbReplay)
+                nbReplay += 1
+                if nbReplay > 40:
+                    continue
+                attributes = record['messageAttributes']
+                attributes.update({'sqs-dlq-replay-nb': {'StringValue': str(nbReplay), 'DataType': 'Number'}})
+                _sqs_attributes_cleaner(attributes)
+                self.sqs.send_message(
+                    QueueUrl=os.environ['QUEUE_URL'],
+                    MessageBody=record['body'],
+                    MessageAttributes=record['messageAttributes'],
+                    MessageGroupId=record['attributes']['MessageGroupId'],
+                    MessageDeduplicationId=record['attributes']['MessageDeduplicationId']
+                )
+        return {}
+
+
+@error_response
+def lambda_handler(event, context):
+    return EventTaskQueueSegmentationClass().handle(event=event,context=context)
+
+def _sqs_attributes_cleaner(attributes):
+    d = dict.fromkeys(attributes)
+    for k in d:
+        if isinstance(attributes[k], dict):
+            subd = dict.fromkeys(attributes[k])
+            for subk in subd:
+                if not attributes[k][subk]:
+                    del attributes[k][subk]
+                else:
+                    attributes[k][''.join(subk[:1].upper() + subk[1:])] = attributes[k].pop(subk)
