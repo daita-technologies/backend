e34683ee50f9074b2e08356f0ab0edae491d6068
BEdaita <daitaservices@gmail.com>
2022-10-31 15:11:17 +0000

    fix: get info proj

--- annotation-app/project-service/api-handler-functions/get-project-info/hdler_get_project_info.py
+++ annotation-app/project-service/api-handler-functions/get-project-info/hdler_get_project_info.py
@@ -8,6 +8,7 @@ import os
 from boto3.dynamodb.conditions import Key, Attr
 from utils import convert_response, aws_get_identity_id, dydb_get_project_full
 from error_messages import *
+from response import *
 
 from models.annotaition.anno_project_model import AnnoProjectModel
 from models.annotaition.anno_project_sum_model import AnnoProjectSumModel
@@ -73,6 +74,6 @@ class GetProjectInfoClass(LambdaBaseClass):
                                 "success": True,
                                 "message": None })
 
-
+@error_response
 def lambda_handler(event, context):
     return GetProjectInfoClass().handle(event=event,  context=context)
